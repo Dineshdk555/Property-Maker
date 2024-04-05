6cc524645a5f5e1697e3923bc7905feaecd693db
Dinesh DK <dk18026@gmail.com>
2024-04-05 18:04:18 +0530

    Backend connect with frontend

--- Property Maker/Client/src/App.js
+++ Property Maker/Client/src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import LogIn from "./components/auth/LogIn";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+    useEffect(() => {
+        // Fetch data from the backend when the component mounts
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/data");
+            console.log("DK Response:",response);
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
   return (
     <>
     <div id="main-app">
--- Property Maker/Server/server.js
+++ Property Maker/Server/server.js
@@ -11,5 +11,5 @@ app.get("/message", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port 8000.`);
+  console.log(`Server is running on port 8080.`);
 });
