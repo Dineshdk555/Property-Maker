62c17bfd720136ba6d178e74627f6cb288a2478d
Dinesh DK <dk18026@gmail.com>
2024-04-05 16:52:11 +0530

    Nodemon update
    BE nodemon update

---
+++ Property Maker/Server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from Property Maker server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port 8000.`);
+});
