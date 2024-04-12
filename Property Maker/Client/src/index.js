eac89715c4cb7bd961ad55fc94c02c36b9dcf058
Dinesh DK <dk18026@gmail.com>
2024-04-12 13:42:42 +0530

    FE-Footer UI Implemented

--- Property Maker/Client/src/index.js
+++ Property Maker/Client/src/index.js
@@ -5,10 +5,14 @@ import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/auth/Header";
+import Footer from "./components/common/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BrowserRouter>
   <React.StrictMode>
+    <Header />
     <App />
-    <ToastContainer/> 
+    <Footer />
+    <ToastContainer />
   </React.StrictMode></BrowserRouter>
 );
